ee1d27adfb6eecf3925fe8a80d7323af7d7520f4
andisabarx <sabarkar1208@gmail.com>
2024-04-21 23:41:19 +0800

    halo

---
+++ src/page/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-container">
+    <div class="fitur-login">
+      <div class="form-login">
+        <div class="sistem-pemeringkatan">
+          <span class="sistem">Sistem</span
+          ><span class="pemeringkatan"> Pemeringkatan </span>
+        </div>
+        <div class="logo-unhas"></div>
+        <div class="from-inputs">
+          <div class="email-inputs">
+            <span class="email-adress-username">EMAIL ADRESS / USERNAME</span>
+            <div class="input-fields">
+              <input type="email" class="email-input" placeholder="Enter your email address">
+            </div>
+          </div>
+          <div class="pass-inputs">
+            <span class="password-text">PASSWORD</span>
+            <div class="input-fields">
+            <input type="password" class="password-input" placeholder="Enter your password"> 
+          </div> 
+          </div>
+          <div class="login-inputs">
+            <button class="login-button"><div class="vector"></div></button
+            ><span class="login-text">LOGIN</span>
+          </div>
+        </div>
+      </div>
+      <div class="login-image"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="javascript"></script>
+
+<style src="./index.css"></style>
